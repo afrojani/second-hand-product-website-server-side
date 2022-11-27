fcd01c11d19c98f46b17d067eb05d7fcc74a6ea6
afrojani <sultana.ani2019@gmail.com>
2022-11-27 02:49:19 -0600

    category api set

--- index.js
+++ index.js
@@ -9,8 +9,41 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7mc1um4.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const carsCollection = client.db('carData').collection('carsCollections');
+        const carsCategory = client.db('carData').collection('category');
+
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const cursor = carsCategory.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get('/categories/:id', async (req, res) => {
+            const nid = parseInt(req.params.id);
+            // console.log(nid);
+            const query = { id: nid };
+            const cursor = carsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
 
 
 
