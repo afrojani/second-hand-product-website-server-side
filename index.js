a0a63c28ea1c6949f84a2e6919110c9c1cfa823c
afrojani <sultana.ani2019@gmail.com>
2022-11-28 07:35:32 -0600

    make admin api set

--- index.js
+++ index.js
@@ -73,6 +73,19 @@ async function run() {
             const query = {};
             const result = await usersCollection.find(query).toArray();
             res.send(result);
+        });
+
+        app.put('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
         })
 
 
